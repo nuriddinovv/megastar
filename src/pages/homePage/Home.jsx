b5500cb3403b8836ae89268d2f03655b99a8e32f
nuriddinovv <blackscarp1onoff@gmail.com>
2024-09-13 13:58:32 +0500

    logo updated

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,20 +1,33 @@
 import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
+import {
+  TbNumber12Small,
+  TbNumber25Small,
+  TbShoppingCartCog,
+  TbShoppingCartDollar,
+  TbShoppingCartX,
+} from "react-icons/tb";
+import { IoReceiptOutline } from "react-icons/io5";
+import { FaCashRegister, FaUsers } from "react-icons/fa";
+import { BsBasket3 } from "react-icons/bs";
+import { LiaSitemapSolid } from "react-icons/lia";
+import { TfiWallet } from "react-icons/tfi";
+import { LuWarehouse } from "react-icons/lu";
 
 export default function Home() {
   const modules = [
-    { path: "/", text: "Продажа" },
-    { path: "/", text: "Чеки" },
-    { path: "/", text: "Отложка" },
-    { path: "/", text: "Клиенты" },
-    { path: "/products", text: "Товары" },
-    { path: "/", text: "Возврат" },
-    { path: "/", text: "Зарплата Менеджеров" },
-    { path: "/", text: "" },
-    { path: "/", text: "Оплата" },
-    { path: "/", text: "Склад " },
-    { path: "/", text: "Просроченные чеки" },
-    { path: "/", text: "Жалобы клиентов" },
+    { path: "/", text: "Продажа", logo: <TbShoppingCartDollar /> },
+    { path: "/", text: "Чеки", logo: <IoReceiptOutline /> },
+    { path: "/", text: "Отложка", logo: <TbShoppingCartCog /> },
+    { path: "/", text: "Клиенты", logo: <FaUsers /> },
+    { path: "/products", text: "Товары", logo: <BsBasket3 /> },
+    { path: "/", text: "Возврат", logo: <TbShoppingCartX /> },
+    { path: "/", text: "Зарплата Менеджеров", logo: <LiaSitemapSolid /> },
+    { path: "/", text: "", logo: <FaCashRegister /> },
+    { path: "/", text: "Оплата", logo: <TfiWallet /> },
+    { path: "/", text: "Склад ", logo: <LuWarehouse /> },
+    { path: "/", text: "Просроченные чеки", logo: <TbNumber25Small /> },
+    { path: "/", text: "Жалобы клиентов", logo: <TbNumber12Small /> },
   ];
   return (
     <div>
@@ -30,7 +43,7 @@ export default function Home() {
                 to={`${item.path}`}
                 className="flex flex-col justify-between items-center w-full h-full"
               >
-                <img src="https://www.picsum.photos/200/200" alt="" />
+                <span className="text-[180px]">{item.logo}</span>
                 <h1>{item.text}</h1>
               </Link>
               <div className=""></div>
